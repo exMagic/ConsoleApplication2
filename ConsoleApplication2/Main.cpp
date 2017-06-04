cd0dc700045f914e9517d03ab11092c92723a337
Maciek <Maciek@PC>
2017-06-04 16:31:46 +0200

    odc. 4

--- ConsoleApplication2/Main.cpp
+++ ConsoleApplication2/Main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include "pytanie.h"
+#include "event.h"
 
 
 using namespace std;
@@ -9,21 +9,8 @@ using namespace std;
 
 int main() {
 	int a;
-
-	Pytanie p[5];
-	int suma = 0;
-	for ( int i = 0; i <= 4; i++)
-	{
-		p[i].nr_pytania = i + 1;
-		p[i].wczytaj();
-		p[i].zadaj();
-		p[i].sprawdz();
-		suma += p[i].punkt;
-	}
-
-
-	cout << "Koniec! punkty: " << suma;
-
-	cin >> a;
+	Event w1("urodziny", 5);
+	w1.show();
+	cin>>a;
 	return 0;
 }
