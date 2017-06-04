9525403cce7d8573399afb22a3b191d32164b87d
Maciek <Maciek@PC>
2017-06-04 20:58:15 +0200

    odc 4 koniec

--- ConsoleApplication2/Main.cpp
+++ ConsoleApplication2/Main.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 #include <string>
-#include "event.h"
+#include "przyjaciele.h"
 
 
 using namespace std;
 
 
+void sedzia(Punkt pkt, Prostokat p) {
+	if ((pkt.x >= p.x) && (pkt.x <= p.x + p.szerokosc) && (pkt.y >= p.y) && (pkt.y <= p.y + p.wysokosc))
+		cout << endl << "Punkt " << pkt.nazwa << " nalezy do rodziny " << p.nazwa;
+	else
+		cout << endl << "Punkt " << pkt.nazwa << " NIENALEZY do rodziny " << p.nazwa;
+}
 
 int main() {
 	int a;
-	Event w1("urodziny", 5);
-	w1.show();
-	cin>>a;
+	//
+	Punkt pkt1("Mike", 3, 18);
+	pkt1.wczytaj();
+
+	Prostokat p1("Corleone", 0, 0, 6, 4);
+	p1.wczytaj();
+
+	sedzia(pkt1, p1);
+
+	//
+	cin >> a;
 	return 0;
 }
---
+++ ConsoleApplication2/przyjaciele.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include "przyjaciele.h"
+
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////////////////
+Punkt::Punkt(string n, float xx, float yy){
+	nazwa = n;
+	x = xx;
+	y = yy;
+}
+void Punkt::wczytaj() {
+	cout << "Podaj x punktu: "; cin >> x;
+	cout << "Podaj y punktu: "; cin >> y;
+	cout << "Nazwa punktu: "; cin >> nazwa;
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+Prostokat::Prostokat(string n, float xx, float yy, float s, float w)
+{
+	nazwa = n;
+	x = xx;
+	y = yy;
+	szerokosc = s;
+	wysokosc = w;
+}
+void Prostokat::wczytaj() {
+	cout << "Podaj x naroznika: "; cin >> x;
+	cout << "Podaj y naroznika: "; cin >> y;
+	cout << "Podaj szerokosc: "; cin >> szerokosc;
+	cout << "Podaj wysokosc: "; cin >> wysokosc;
+	cout << "Podaj nazwe prostokata: "; cin >> nazwa;
+
+
+}
+
